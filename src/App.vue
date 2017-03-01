49d1096d658ab674c1c3ff7f83b0b537cb8069ac
toast38coza <info@38.co.za>
2017-03-01 19:48:31 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h4>Usage</h4>
+    <pre>
+      &lt;text-editable v-model='content' &gt;&lt;/text-editable&gt;
+    </pre>
+
+    <h4>Example</h4>
+    <div>
+      <strong>Editable text: (you can edit in place)</strong>
+      <text-editable v-model='content' ></text-editable>
+      <hr/>
+      <strong>Edited text:</strong>
+      <p>{{content}}</p>
+    </div>
+    <!--
+    <div>
+      <h5>Set <code>output</code> to <code>html</code></h5>
+      <strong>Editable text:</strong>
+      <text-editable v-model='htmlContent' output='html' ></text-editable>
+      <hr/>
+      <strong>Edited text:</strong>
+      <p v-html='htmlContent'></p>
+    </div>
+    -->
+  </div>
+</template>
+
+<script>
+import TextEditable from './components/TextEditable'
+
+export default {
+  name: 'app',
+  components: {
+    TextEditable
+  },
+  data () {
+    return {
+      content: 'hello world',
+      htmlContent: 'hello <b>world</b>'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TextEditable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    :id='id'
+    @keyup='contentChanged'
+    contenteditable="true">{{content}}</div>
+</template>
+
+<script>
+export default {
+  name: 'TextEditable',
+  props: {
+    value: { type: String, required: true },
+    output: { type: String, default: 'text' }
+  },
+  data () {
+    return {
+      content: this.value,
+      id: Math.random().toString().split('.')[0]
+    }
+  },
+  methods: {
+    contentChanged () {
+      let el = document.getElementById(this.id)
+      this.$emit('input', el.innerHTML)
+      /*
+      if (this.output === 'html') {
+        console.log(el.innerHTML)
+        this.$emit('input', el.innerHTML)
+      } else {
+        this.$emit('input', el.textContent)
+      }
+      */
+    }
+  }
+}
+</script>
